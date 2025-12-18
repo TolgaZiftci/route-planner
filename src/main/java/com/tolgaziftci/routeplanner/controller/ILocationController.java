42171bf676f2ef12c1b0af4f3b2c8bbc64222c39
Tolga Ziftci <tolgazif@hotmail.com>
2025-12-18 21:28:11 +0300

    Fix typos in location controller doc

--- src/main/java/com/tolgaziftci/routeplanner/controller/ILocationController.java
+++ src/main/java/com/tolgaziftci/routeplanner/controller/ILocationController.java
@@ -22,7 +22,7 @@ import java.util.List;
 public interface ILocationController {
 
     @Operation(
-            summary = "Get a locationRequest by its ID",
+            summary = "Get a location by its ID",
             parameters = {@Parameter(name = "id", description = "Location ID")},
             tags = {"get"}
     )
@@ -32,7 +32,7 @@ public interface ILocationController {
     )
     @ApiResponse(
             responseCode = "204",
-            description = "If a locationRequest with the given ID does not exist"
+            description = "If a location with the given ID does not exist"
     )
     @GetMapping("/{id}")
     public ResponseEntity<LocationDao> findLocationById(@PathVariable int id);
@@ -50,48 +50,48 @@ public interface ILocationController {
     public ResponseEntity<List<LocationDao>> getAllLocations();
 
     @Operation(
-            summary = "Add a new locationRequest to the database",
+            summary = "Add a new location to the database",
             tags = {"post"}
     )
     @io.swagger.v3.oas.annotations.parameters.RequestBody(
             description = """
                     name: Location name
                     
-                    country: Country of locationRequest
+                    country: Country of location
                     
-                    city: City of locationRequest
+                    city: City of location
                     
-                    locationCode: The code for the locationRequest (IATA airport codes are used for airports)
+                    locationCode: The code for the location (IATA airport codes are used for airports)
                     """
     )
     @ApiResponse(
             responseCode = "400",
-            description = "If the locationRequest already exists or any field is empty"
+            description = "If the location already exists or any field is empty"
     )
     @ApiResponse(
             responseCode = "200",
-            description = "Full specification of the new locationRequest including ID",
+            description = "Full specification of the new location including ID",
             useReturnTypeSchema = true
     )
     @PostMapping
     public ResponseEntity<LocationDao> addLocation(@RequestBody LocationRequest location);
 
     @Operation(
-            summary = "Delete a locationRequest from the database",
-            parameters = {@Parameter(name = "id", description = "ID of the locationRequest to delete")},
+            summary = "Delete a location from the database",
+            parameters = {@Parameter(name = "id", description = "ID of the location to delete")},
             tags = {"delete"}
     )
     @ApiResponse(
             responseCode = "404",
-            description = "If the locationRequest does not exist"
+            description = "If the location does not exist"
     )
     @ApiResponse(
             responseCode = "400",
-            description = "If the locationRequest has transportations attached"
+            description = "If the location has transportations attached"
     )
     @ApiResponse(
             responseCode = "200",
-            description = "If the locationRequest was successfully deleted"
+            description = "If the location was successfully deleted"
     )
     @DeleteMapping("/{id}")
     public ResponseEntity<LocationDao> removeLocation(@PathVariable int id);
